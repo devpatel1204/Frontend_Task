9a2926408784bfdfd73e6aa128cdffdef80b6bf8
devpatel1204 <devp84630@gmail.com>
2023-03-05 19:40:53 +0530

    update employee field

--- src/Compnents/JobFormPage2.jsx
+++ src/Compnents/JobFormPage2.jsx
@@ -50,7 +50,7 @@ const JobFormPage2 = ({ formData, setFormData }) => {
                                             </div>
                                         </div>
                                         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" name="totalEmployee" value={formData.totalEmployee} onChange={(e) => { handleOnChange(e) }} placeholder="ex. 100">Total Employee</label>
-                                        <input type="text" class="bg-gray-50 mb-6 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="totalEmployee" value={formData.totalEmployee} onChange={(e) => { handleOnChange(e) }} placeholder="ex. 100" required />
+                                        <input type="text" class="bg-gray-50 mb-6 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="totalEmployee" value={formData.totalEmployee} onChange={(e) => { handleOnChange(e) }} placeholder="ex. 100" />
                                     </div>
                                 </div>
                                 <label class="block mb-1 text-sm font-medium text-gray-900 dark:text-white" >Apply type</label>
